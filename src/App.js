cbb2e3541d935a8e6b174844b8ac39ac5b0bf181
Momanyi Poet <101427637+MomanyiPoet@users.noreply.github.com>
2023-03-15 17:25:13 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="box">
+        <span className="border-line"></span>
+        <form action="">
+          <div className="up">
+            <h2>LOGIN</h2>
+          </div>
+          <div className="input-box">
+            <input type="text" required />
+            <span>Username</span>
+            <i></i>
+          </div>
+
+          <div className="input-box">
+            <input type="password" required />
+            <span>Password</span>
+            <i></i>
+          </div>
+
+          <input type="submit" value="Sign In" />
+
+          <div className="links">
+            <a href="#">Forgot Password</a>
+            <a href="#">Signup</a>
+          </div>
+        </form>
+      </div>
+
+      <div className="combine">
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+        <div className="square"></div>
+
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+
+        <div className="triangle"></div>
+        <div className="triangle"></div>
+        <div className="triangle"></div>
+        <div className="triangle"></div>
+        <div className="triangle"></div>
+      </div>
     </div>
   );
 }
